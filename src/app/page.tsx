0314579c5e5712b0f9944bee763ff54a36605051
zerotowerszero-cyber <zerotowerszero@gmail.com>
2026-09-12 12:25:18 -0400

    Restyle part 1/4: tokens, page shell, geist dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Features from "@/components/Features";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
 import About from "@/components/About";
@@ -12,8 +13,9 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <Hero />
-        <Work />
+        <Features />
         <Services />
+        <Work />
         <About />
         <Contact />
       </main>
